baf558954dca716e42b37c64eabbb2640ff810dc
Mehmet Nadi <mehmetnadi@Mehmets-MBP.lan>
2021-08-07 12:01:25 -0700

    make IntroSection.jsx responsive

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -9,34 +9,45 @@ export default function Btn(props) {
            <button id={`nav-btn-${props.id}`}>{props.btnText}
 
 
-<style jsx>{`
+                <style jsx>{`
 
 
-${`#nav-btn-${props.id}`} {
-    background-color: ${props.btnColor};
-    border-style: solid;
-    border-width: 0.5px;
-    border-color: ${props.borderColor === undefined? props.btnColor: props.borderColor}; 
-    {/* ^^^^above means^^^^ if the borderColor is null, then set border-color as btnColor. This is usefel for cases where the btn has a different border color than the btn body. this only once happens in the website, thou */}
-    font-size: 16px;
-    min-width: 40%;
-    border-radius: 25px;
-    min-height: 30px;
-    color: ${props.textColor}
-    
-    
+                ${`#nav-btn-${props.id}`} {
+                    background-color: ${props.btnColor};
+                    border-style: solid;
+                    border-width: 0.5px;
+                    border-color: ${props.borderColor === undefined? props.btnColor: props.borderColor}; 
+                    {/* ^^^^above means^^^^ if the borderColor is null, then set border-color as btnColor. This is usefel for cases where the btn has a different border color than the btn body. this only once happens in the website, thou */}
+                    font-size: 16px;
+                    min-width: 40%;
+                    border-radius: 25px;
+                    min-height: 30px;
+                    color: ${props.textColor};
+                    margin: 10px auto;
+                    
+                    
 
-}
-${`#nav-btn-${props.id}:hover`} {
-    background-color: ${props.btnHoverColor};
-    cursor: pointer
-    
-}
+                }
+                ${`#nav-btn-${props.id}:hover`} {
+                    background-color: ${props.btnHoverColor};
+                    cursor: pointer
+                    
+                }
+
+                @media only screen and (max-width: 900px) {
+
+                    ${`#nav-btn-${props.id}`} {
+                        min-width: 90%;
+
+
+                    }
+
+                }
 
 
 
-`}</style>
-           
+                `}</style>
+                        
            
            </button>
 
--- src/components/IntoSection.jsx
+++ src/components/IntoSection.jsx
@@ -9,15 +9,15 @@ export default function IntoSection() {
                 <h1 className='intro-title'>Create a Secret Santa</h1>
                 <p className="intro-sentence">Match Santas randomly. Noday knows who got who!</p>
                 <div className="into-action-btn-container">
-                <Btn id={3} btnColor="#D00000" btnHoverColor="#FF6584" btnText="Sign Up" textColor="white" ></Btn>
-                <Btn id={4} btnColor="#FFFFFF" btnHoverColor="#FF6584" btnText="Sign Up" textColor="black" borderColor="#D00000" ></Btn>
+                    <Btn id={3} btnColor="#D00000" btnHoverColor="#FF6584" btnText="Sign Up" textColor="white" ></Btn>
+                    <Btn id={4} btnColor="#FFFFFF" btnHoverColor="#FF6584" btnText="Sign Up" textColor="black" borderColor="#D00000" ></Btn>
 
                 </div>
             </div>
 
 
             <div className="img-container">
-                <img src="../Santa.svg" alt="" />
+                <img id="home-page-img" src="../Santa.svg" alt="" />
             </div>
 
 
@@ -31,11 +31,13 @@ export default function IntoSection() {
                     border-style: solid;
                     border-width: 2px;
                     border-color: red;
+                    box-sizing: border-box;
                     
-                    min-width: 100vh;
+                    width: 100%;
                     min-height: 100vh;
                     align-items: center;
                     display: flex;
+                    flex-wrap: wrap-reverse	;
                     justify-content: center;
                     
                 }
@@ -63,7 +65,7 @@ export default function IntoSection() {
                 .intro-sentence {
                     font-family: Quicksand;
                     font-style: normal;
-                    font-weight: 500;
+                    font-weight: 500px;
                     font-size: 30px;
                     line-height: 37px;
                 }
@@ -81,12 +83,45 @@ export default function IntoSection() {
                     border-width: 2px;
                     border-color: pink;
                     width: 50%;
-                    min-width: 750px;
-                    
-
                     margin: 0 auto;
                 
+                }
+                #home-page-img{
+                    max-width: 100%;
+                }
+
+
+                @media only screen and (max-width: 900px) {
+                    {/* breaking point is 900px */}
+
+                    .Intro-section-wrapper {
+                        max-width: 100%;
+                    }
+
+                    .content-container  {
+                        width: 100%;
+
+                    }
+
+                    .img-container{
+                        width: 100%;        
+
+                    }
                     
+                    .intro-title {
+                        
+                    font-size: 40px;
+
+                    color: #D00000;
+                }
+                .intro-sentence {
+                    font-size: 20px;
+                }
+                .into-action-btn-container{
+                    flex-wrap: wrap;
+                }
+
+
                 }
 
                 
